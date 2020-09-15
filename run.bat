7088a3d7022cbd8fe1712c9db4c9e4468b864fde
Prashanthreddy361 <70228975+Prashanthreddy361@users.noreply.github.com>
2020-09-15 19:51:28 +0530

    Update run.bat
    updated run bat file regression scenarios

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 pytest -s -v -m "sanity" --html=./Reports/report_sanity.html testCases/ --browser chrome
-
+pytest -s -v -m "regression" --html=./Reports/report_sanity.html testCases/ --browser chrome
 
 cmd /k
