23513c3e65f92c7b9736dea13412c1b12e9a8da9
Prashanthreddy361 <prashureddy010@gmail.com>
2020-09-15 19:56:22 +0530

    this is my second commit

--- run.bat
+++ run.bat
@@ -1,4 +1,2 @@
 pytest -s -v -m "sanity" --html=./Reports/report_sanity.html testCases/ --browser chrome
 pytest -s -v -m "regression" --html=./Reports/report_sanity.html testCases/ --browser chrome
-
-cmd /k
